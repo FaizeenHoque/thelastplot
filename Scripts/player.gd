3bd58065f464ad0347afb23d89dca55149224cda
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-09-15 22:05:30 +0600

    Implemented Slashing/Attacking Animation Handling: Damaging not yet implemented

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
 const SPEED = 150.0
+
 var last_direction: Vector2 = Vector2.DOWN
+var is_slashing: bool = false
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("Slash") and not is_slashing:
+		slash()
+	
+	# skip movement if attacking
+	if is_slashing:
+		velocity = Vector2.ZERO
+		return
+		
 	process_movement()
 	move_and_slide()
 
@@ -20,6 +30,8 @@ func process_movement() -> void:
 	process_animation(last_direction)
 
 func process_animation(direction: Vector2) -> void:
+	if is_slashing:
+		return
 	if velocity != Vector2.ZERO:
 		play_animation("walk", direction)
 	else:
@@ -33,3 +45,12 @@ func play_animation(prefix: String, dir: Vector2) -> void:
 		animated_sprite_2d.play(prefix + "_up")
 	elif dir.y > 0:
 		animated_sprite_2d.play(prefix + "_down") 
+
+func slash() -> void:
+	is_slashing = true
+	play_animation("slash", last_direction)	
+	print("Attack")
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if is_slashing:
+		is_slashing = false
