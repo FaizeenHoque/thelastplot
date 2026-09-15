c705adfa53e76ebfa32085591f49b84030550001
Faizeen Hoque <haquefaizeen11@gmail.com>
2026-09-15 21:23:38 +0600

    Downloaded a asset pack and changed character sprite animations

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 150.0
 var last_direction: Vector2 = Vector2.DOWN
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
@@ -26,11 +26,10 @@ func process_animation(direction: Vector2) -> void:
 		play_animation("idle", direction)
 
 func play_animation(prefix: String, dir: Vector2) -> void:
-	if dir.x < 0:
-		animated_sprite_2d.play(prefix + "_right")
-	elif dir.y < 0:
-		animated_sprite_2d.play(prefix + "_top")
-	elif dir.x > 0:
+	if dir.x != 0:
+		animated_sprite_2d.flip_h = dir.x < 0 
 		animated_sprite_2d.play(prefix + "_left")
+	elif dir.y < 0:
+		animated_sprite_2d.play(prefix + "_up")
 	elif dir.y > 0:
 		animated_sprite_2d.play(prefix + "_down") 
